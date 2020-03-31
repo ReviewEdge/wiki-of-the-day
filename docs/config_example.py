653d57361fbc140a459a6aed8ba111b11dbb50f5
Logan Richardson <reviewedge56@gmail.com>
2020-03-30 23:02:52 -0400

    Create config_example.py

---
+++ docs/config_example.py
@@ -0,0 +1,10 @@
+# When you create your own configuration file, make sure it is named "config.py"
+
+# wiki of the day info:
+test_email_address = "XXXXX"
+test_email_password = "XXXXX"
+
+email_send_list_google_sheet_id = "XXXXX"
+
+send_wiki_email_address = "XXXXX"
+send_wiki_email_password = "XXXXX"
